90c7f0e895e5aa26d67115460f31ecc14b71a245
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-05-23 10:05:46 -0400

    remove-element

---
+++ remove-element/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+	let index = nums.length;
+	let count = 0;
+	while (index > -1) {
+		if (nums[index] === val) {
+			nums.splice(index, 1);
+			count++;
+		}
+		index--;
+	}
+	console.log(count + nums);
+};

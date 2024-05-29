687e5b1771ee4923c32cf30d83d63d71a4215ba7
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-05-29 12:37:30 -0400

    optimize compress

--- compress/index.js
+++ compress/index.js
@@ -2,28 +2,18 @@ const compress = (s) => {
 	let string = "";
 	let k = 0;
 	let j = 0;
-	let count = 0;
-	let total = 0;
 
-	if (s.length === 1) {
-		return "1" + s;
-	}
-
-	while (total < s.length) {
+	while (k <= s.length) {
 		if (s[j] === s[k]) {
 			k += 1;
-			count += 1;
 		} else {
+			let count = k - j;
 			if (count === 1) {
 				string += s[j];
 			} else {
 				string += count + s[j];
 			}
-
-			total += count;
-			j = total;
-			k = total;
-			count = 0;
+			j = k;
 		}
 	}
 	return string;

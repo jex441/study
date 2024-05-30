4b360e5cdfb7366b9c3e064bdb7621bc6aa9ce29
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-05-30 11:29:18 -0400

    anagrams

---
+++ anagram/index.js
@@ -0,0 +1,30 @@
+const anagrams = (s1, s2) => {
+	// make a hash map
+	let count = {};
+	for (let char of s1) {
+		if (!count[char]) {
+			count[char] = 1;
+		} else {
+			count[char] += 1;
+		}
+	}
+	for (let char of s2) {
+		if (!count[char]) {
+			return false;
+		} else {
+			count[char] -= 1;
+		}
+	}
+
+	for (let char in count) {
+		if (count[char] !== 0) {
+			return false;
+		}
+	}
+	return true;
+	console.log(count);
+};
+
+module.exports = {
+	anagrams,
+};

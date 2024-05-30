66d3aebf9b1e662ebc16d513dd2443b0dd4a2c1b
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-05-30 12:22:28 -0400

    pairsum

--- pairsum/index.js
+++ pairsum/index.js
@@ -1,11 +1,25 @@
+//Hashmap:
 const pairSum = (numbers, targetSum) => {
-	let result = [];
+	let count = {};
 	for (let i = 0; i < numbers.length; i++) {
-		for (let j = 0; j < numbers.length; j++) {
-			if (i !== j && numbers[i] + numbers[j] === targetSum) {
-				result.push(i, j);
-				return result;
-			}
+		let compliment = targetSum - numbers[i];
+		if (compliment in count) {
+			return [count[compliment], i];
+		} else {
+			count[numbers[i]] = i;
 		}
 	}
 };
+
+// Brute force:
+// const pairSum = (numbers, targetSum) => {
+// 	let result = [];
+// 	for (let i = 0; i < numbers.length; i++) {
+// 		for (let j = 0; j < numbers.length; j++) {
+// 			if (i !== j && numbers[i] + numbers[j] === targetSum) {
+// 				result.push(i, j);
+// 				return result;
+// 			}
+// 		}
+// 	}
+// };

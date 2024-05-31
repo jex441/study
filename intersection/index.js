dd9fbb4c163cdb8a0b97ac3d66ca55bea2c8b57c
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-05-30 20:32:48 -0400

    intersection w set

--- intersection/index.js
+++ intersection/index.js
@@ -1,25 +1,16 @@
 const intersection = (a, b) => {
-	// todo
-	// make a hashmap
-	let map = {};
+	let set = new Set();
 	let result = [];
 
 	for (let char of a) {
-		map[char] = 1;
+		set.add(char);
 	}
 
 	for (let char of b) {
-		if (!map[char]) {
-			map[char] = 1;
-		} else {
-			map[char] += 1;
+		if (set.has(char)) {
+			result.push(char);
 		}
 	}
 
-	for (let key in map) {
-		if (map[key] > 1) {
-			result.push(Number(key));
-		}
-	}
 	return result;
 };

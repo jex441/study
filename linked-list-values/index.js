0f856caaf307b49d8ff9d2bc2fe582452794eacb
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-06-04 10:20:53 -0400

    linked list values recursive

--- linked-list-values/index.js
+++ linked-list-values/index.js
@@ -6,3 +6,15 @@ const linkedListValues = (head) => {
 	}
 	return result;
 };
+
+const linkedListValuesRecursive = (head) => {
+	const result = [];
+	_linkedListValuesRecursive(head, result);
+	return result;
+};
+
+const _linkedListValuesRecursive = (head, result) => {
+	if (head === null) return;
+	result.push(head.val);
+	_linkedListValuesRecursive(head.next, result);
+};

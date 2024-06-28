5835fe5f47dd05f4f12f0b65f7f1a0e114b036bb
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-06-28 10:39:25 -0400

    bfs

--- has-path/index.js
+++ has-path/index.js
@@ -26,3 +26,20 @@ const _hasPath = (graph, src, dst) => {
 
 	return false;
 };
+
+// Breadth First Search
+const __hasPath = (graph, src, dst) => {
+	// declare a queue
+	let queue = [src];
+	while (queue.length > 0) {
+		let current = queue.shift();
+		for (let neighbor of graph[current]) {
+			if (neighbor === dst) {
+				return true;
+			} else {
+				queue.unshift(neighbor);
+			}
+		}
+	}
+	return false;
+};
